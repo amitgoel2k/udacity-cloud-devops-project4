f1a6f34b8117f3e874ba02cdf1970179e74adfa4
Amit Goel <amitgoel.2k@gmail.com>
2022-11-07 16:55:40 +0530

    Updated run_kubernetes.sh & kubernetes_output.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=amitgoel2k/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-api --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-api 8080:80
