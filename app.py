6e6495a16c1c6a54ba118c730747135690354e48
Amit Goel <amitgoel.2k@gmail.com>
2022-11-07 10:11:39 +0000

    added log statement in app.py

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
+    # Log the output prediction value
+    LOG.info(f"output prediction: \n{prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
